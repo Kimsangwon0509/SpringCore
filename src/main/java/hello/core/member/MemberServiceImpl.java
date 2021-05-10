75d561186e48b6e03492a64cc0a77a28bada6420
Kimsangwon <kimsw509@naver.com>
2021-05-10 13:56:19 +0900

    잠깐: :boom: 추상화와 구체화에 둘다 의존하는 안좋은 코드 git add .

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,9 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    //현재의 코드가 MemberRepository라는 추상화에도 의존하고 emoryMemberRepository라는 구체화에도 의존하므로 안좋은 코드가 된다.
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+
 
     @Override
     public void join(Member member) {
