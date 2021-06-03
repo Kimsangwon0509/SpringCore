2b0b9bd03e0e9f18911b92b442f25bd230a1e40a
Kimsangwon <kimsw509@naver.com>
2021-06-03 13:55:31 +0900

    새로운 구조와 할인 정책 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -26,7 +27,8 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+        //return new FixDiscountPolicy();
     }
 
 /*
@@ -49,4 +51,10 @@ public class AppConfig {
      * new MemoryRepository() 부분의 중복이 제거되었다. 이제 MemoryMemberRopository를 다른 구현체로 변경할 때 한 부분만 변경하면 된다
      * AppConfig 를 보면 역활과 구현 클래스가 한눈에 들어온다. 애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악할 수 있다.
      * */
+
+    /*
+    * AppConfig에서 할인 정책 역활을 담당하는 구현을 FixDiscountPolicy에서 RateDiscountPolicy 객체로 변경했다
+    * 이제 할인 정책을 변경해도, 애플리케이션의 구성 역활을 담당하는 AppConfig만 변경하면 된다. 클라이언트 코드인 OrderServiceIml를 포함해서 사용 영역의 어떤 코드도 변경할 필요가 없다
+    * 구성 영역(AppConfig)은 당연히 변경된다. 구성 역활을 담당하는 AppConfig를 애플리케이션이라는 공연의 기획자로 생각. 공연 기획자는 공연 참여자인 구현 객체들을 모두 알아야 한다.
+    * */
 }
