c7b353ef175bcec9af294f7defc295aa168373f7
Kimsangwon <kimsw509@naver.com>
2021-06-29 22:28:48 +0900

    싱글톤 컨테이너

--- src/test/java/hello/core/singelton/SingletonTest.java
+++ src/test/java/hello/core/singelton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -48,4 +50,26 @@ public class SingletonTest {
         //same ==
         //equal 은 자바의 equals메서드를 오버라이딩 하는것.
     }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+
+        //AppConfig config = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+
+        //참조 값이 다른것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //항상 자동화 해 놓을것
+        //memberService1 != memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+
+        //스프링 빈(스프링 컨테이너)은 99퍼센트 싱글톤 방식으로 동작한다
+    }
+
 }
