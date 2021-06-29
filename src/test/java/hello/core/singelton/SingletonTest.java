09fcf95b8038c4c914588e690f0d89adfa61ebaf
Kimsangwon <kimsw509@naver.com>
2021-06-29 22:12:17 +0900

    싱글톤 패턴

--- src/test/java/hello/core/singelton/SingletonTest.java
+++ src/test/java/hello/core/singelton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SingletonTest {
 
     @Test
@@ -30,6 +32,20 @@ public class SingletonTest {
 
         //항상 자동화 해 놓을것
         //memberService1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonService() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        //same ==
+        //equal 은 자바의 equals메서드를 오버라이딩 하는것.
     }
 }
